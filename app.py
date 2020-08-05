f00997de87c9b5c2637eb1a2fb95775670908629
MatthewBailey97 <45615603+MatthewBailey97@users.noreply.github.com>
2020-08-05 11:17:40 -0400

    Final figure creation

--- app.py
+++ app.py
@@ -92,9 +92,9 @@ slider_template = dict(
     )
 
 if(choose_worktype == 'Key metrics'):
-    barFig = go.Figure()
-    barFig.update_layout(covid_template)
-    barFig.update_layout(
+    pos_inc_fig = go.Figure()
+    pos_inc_fig.update_layout(covid_template)
+    pos_inc_fig.update_layout(
         #template=covid_template,
         title="Daily increase of positive cases",
         xaxis_title='Date',
@@ -102,28 +102,138 @@ if(choose_worktype == 'Key metrics'):
         legend_title="Legend",
         xaxis=slider_template
         )
-    barFig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positiveIncrease'],name='positiveIncrease',text=covid_data['positiveIncrease'],textposition='inside',))
+    pos_inc_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positiveIncrease'],name='positiveIncrease',text=covid_data['positiveIncrease'],textposition='inside',))
     
     moving_avg_7 = covid_data['positiveIncrease'].rolling(7).mean()
-    barFig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
+    pos_inc_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
 
-    barFig.update_yaxes(automargin=True)
+    pos_inc_fig.update_yaxes(automargin=True)
     
-    st.plotly_chart(barFig, use_container_width=False)
+    st.plotly_chart(pos_inc_fig, use_container_width=False)
 
 
-#TODO Selectbox Positive Cumulative
+    #TODO Selectbox Positive Cumulative
+    if(st.checkbox("Total Positive")):
+        pos_cumu_fig = go.Figure()
+        pos_cumu_fig.update_layout(covid_template)
+        pos_cumu_fig.update_layout(
+            #template=covid_template,
+            title="Total Positive Cases",
+            xaxis_title='Date',
+            yaxis_title='Positive Cumulative',
+            legend_title="Legend",
+            xaxis=slider_template
+            )
+        pos_cumu_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positive'],name='positive',text=covid_data['positive'],textposition='inside',))
+    
+        moving_avg_7 = covid_data['positive'].rolling(7).mean()
+        pos_cumu_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
 
-#TODO Death Increase
+        pos_cumu_fig.update_yaxes(automargin=True)
+    
+        st.plotly_chart(pos_cumu_fig, use_container_width=False)
 
-#TODO Selectbox Death Cumulative
+    #TODO Death Increase
+    dth_inc_fig = go.Figure()
+    dth_inc_fig.update_layout(covid_template)
+    dth_inc_fig.update_layout(
+        #template=covid_template,
+        title="Daily Increase of Deaths",
+        xaxis_title='Date',
+        yaxis_title='Death Increase',
+        legend_title="Legend",
+        xaxis=slider_template
+        )
+    dth_inc_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['deathIncrease'],name='Death Increase',text=covid_data['deathIncrease'],textposition='inside',))
+    
+    moving_avg_7 = covid_data['deathIncrease'].rolling(7).mean()
+    dth_inc_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
 
-#TODO Hospitalized Increase
+    dth_inc_fig.update_yaxes(automargin=True)
+    
+    st.plotly_chart(dth_inc_fig, use_container_width=False)
+    #TODO Selectbox Death Cumulative
+    if(st.checkbox("Death Cumulative")):
+        dth_cumu_fig = go.Figure()
+        dth_cumu_fig.update_layout(covid_template)
+        dth_cumu_fig.update_layout(
+            #template=covid_template,
+            title="Total Deaths",
+            xaxis_title='Date',
+            yaxis_title='Deaths Cumulative',
+            legend_title="Legend",
+            xaxis=slider_template
+            )
+        dth_cumu_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['death'],name='death',text=covid_data['death'],textposition='inside',))
+    
+        moving_avg_7 = covid_data['death'].rolling(7).mean()
+        dth_cumu_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
 
-#TODO Hospitalized Currently
+        dth_cumu_fig.update_yaxes(automargin=True)
+    
+        st.plotly_chart(dth_cumu_fig, use_container_width=False)
 
-#TODO Selectbox Hospitalized Cumulative 
+    #TODO Hospitalized Increase
+    hosp_inc_fig = go.Figure()
+    hosp_inc_fig.update_layout(covid_template)
+    hosp_inc_fig.update_layout(
+        #template=covid_template,
+        title="Daily Increase of Hospitalizations",
+        xaxis_title='Date',
+        yaxis_title='Hospitalization Increase',
+        legend_title="Legend",
+        xaxis=slider_template
+        )
+    hosp_inc_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['hospitalizedIncrease'],name='Hospitalized Increase',text=covid_data['hospitalizedIncrease'],textposition='inside',))
+    
+    moving_avg_7 = covid_data['hospitalizedIncrease'].rolling(7).mean()
+    hosp_inc_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
 
+    hosp_inc_fig.update_yaxes(automargin=True)
+    
+    st.plotly_chart(hosp_inc_fig, use_container_width=False)
+
+    #TODO Hospitalized Currently
+    hosp_cur_fig = go.Figure()
+    hosp_cur_fig.update_layout(covid_template)
+    hosp_cur_fig.update_layout(
+        #template=covid_template,
+        title="Currently Hospitalized",
+        xaxis_title='Date',
+        yaxis_title='Hospitalized',
+        legend_title="Legend",
+        xaxis=slider_template
+        )
+    hosp_cur_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['hospitalizedCurrently'],name='Death Increase',text=covid_data['hospitalizedCurrently'],textposition='inside',))
+    
+    moving_avg_7 = covid_data['hospitalizedCurrently'].rolling(7).mean()
+    hosp_cur_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
+
+    hosp_cur_fig.update_yaxes(automargin=True)
+    
+    st.plotly_chart(hosp_cur_fig, use_container_width=False)
+
+    #TODO Selectbox Hospitalized Cumulative 
+    if(st.checkbox("Hospitalized Cumulative")):
+        hosp_cumu_fig = go.Figure()
+        hosp_cumu_fig.update_layout(covid_template)
+        hosp_cumu_fig.update_layout(
+            #template=covid_template,
+            title="Total Hospitalizations",
+            xaxis_title='Date',
+            yaxis_title='Hospitalized Cumulative',
+            legend_title="Legend",
+            xaxis=slider_template
+            )
+        hosp_cumu_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['hospitalizedCumulative'],name='positive',text=covid_data['hospitalizedCumulative'],textposition='inside',))
+    
+        moving_avg_7 = covid_data['hospitalizedCumulative'].rolling(7).mean()
+        hosp_cumu_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
+
+        hosp_cumu_fig.update_yaxes(automargin=True)
+    
+        st.plotly_chart(hosp_cumu_fig, use_container_width=False)
+        
 
 
 
