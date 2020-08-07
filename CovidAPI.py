fa9fdb8b1e5121c16e4292cab8f11dd699526480
MatthewBailey97 <45615603+MatthewBailey97@users.noreply.github.com>
2020-08-06 22:50:03 -0400

    Major overhaul and cleanup

--- CovidAPI.py
+++ CovidAPI.py
@@ -13,41 +13,79 @@ class CovidAPI:
     localCurrent = ''
     localDay = ''
 
-    #def __init__(self):
         
     def setDataPath(self, dataPath):
-        #self.localData = pd.read_json(dataPath)
+        """Function takes in path to json file and loads it for use
+
+        Args:
+            dataPath (String): filepath to data file
+        """
         with open(dataPath,"r+") as file:
             self.localData = json.load(file)
 
-    #StateAbrv = State Abbreviation E.g. "ga", "ca"
+
     def requestStateDaily(self, stateAbrv):
+        """Requests historical daily data
+
+        Args:
+            stateAbrv (String): State Abbreviation for API request E.g. "ga", "ca"
+
+        Returns:
+            Json: Returns request data in json format
+        """
         return self._requestData(self.baseURL+self.statesApi+stateAbrv.lower()+"/daily.json")
         
 
-    #StateAbrv = State Abbreviation E.g. "ga", "ca"
     def requestStateCurrent(self, stateAbrv):
+        """Requests most recent data values for a state, typically the values of the previous day
+
+        Args:
+            stateAbrv (String): State Abbreviation for API request E.g. "ga", "ca"
+
+        Returns:
+            Json: Returns request data in json format
+        """
         return self._requestData(self.baseURL+self.statesApi+stateAbrv.lower()+"/current.json")
         
 
     def requestStateDate(self, stateAbrv, date):
+        """Requests data values of a specific date for a state
+
+        Args:
+            stateAbrv (String): State Abbreviation for API request E.g. "ga", "ca"
+            date (String): Date value that is then converted to string of digits E.g. "8/1/2020" to "20200801"
+
+        Returns:
+            Json: Returns request data in json format
+        """
         hDate = date.strftime("%Y%m%d")
         return self._requestData(self.baseURL+self.statesApi+stateAbrv.lower()+"/"+hDate+".json")
 
     def _requestData(self, url):
+        """Handles data request functions for each requst function
+
+        Args:
+            url (String): Takes in string value for covidtracking.com api call
+
+        Returns:
+            Json: Returns request call in Json format
+        """
         req = requests.get(url)
         reqData = req.json()
         return reqData
 
 
-    #Update locally stored JSON file
     def updateData(self, stateAbrv):
+        """Keeps locally stored json file updated with most recent data values
+
+        Args:
+            stateAbrv (String): State Abbreviation for API request E.g. "ga", "ca"
+        """
         dataHold = []
         localDate = self.localData[0]['date']
 
         newData = self.requestStateDaily(stateAbrv)
 
-        #newData_Date = newData[0]['date']
         if newData[0]['date'] != localDate:
             for dic in newData: 
                 if dic['date'] != localDate:
@@ -59,8 +97,3 @@ class CovidAPI:
 
         with open("Georgia_Covid.json",'w') as outfile:
             json.dump(updatedData,outfile)
-        
-        #print(dataHold)
-        #print(type(dataHold)
-        #print(dataHold[0])
-        #print(dataHold[-1])
--- app.py
+++ app.py
@@ -9,9 +9,6 @@ from plotly.subplots import make_subplots
 import plotly.io as pio
 pio.templates.default = "plotly"
 
-st.title('Georgia Covid Analysis')
-st.write("This app is for exploring Georgia's Covid data")
-
 def data_retrieval():
     """Utilizes CovidAPI.py to handle data requests to "covidtracking.com" and
     for keeping the data file updated
@@ -40,14 +37,9 @@ def load_data(filepath):
 
 covid_data = load_data("Georgia_Covid.json")
 st.sidebar.header("Navigation")
-choose_worktype = st.sidebar.selectbox("Go to",['Key metrics','Interactive','Play'])
+sections = st.sidebar.selectbox("Go to",['Home', 'Future Goals'])
 st.sidebar.header("About")
-st.sidebar.info("This app is maintained by Matthew Bailey")
-
-if st.checkbox("Show Data"):
-    st.dataframe(covid_data)
-    st.write("Dataset contains %s columns and %s rows" % (len(covid_data.columns),len(covid_data)))
-
+st.sidebar.info("This app is maintained by Matthew Bailey. The code can be found on [Github](https://github.com/MatthewBailey97/Georgia_Covid)")
 
 
 # Template for plot visuals
@@ -91,29 +83,38 @@ slider_template = dict(
         type="date"
     )
 
-if(choose_worktype == 'Key metrics'):
+if(sections == 'Home'):
+
+    st.title('Georgia Covid Statistics')
+    st.write("This app is for visualizing Georgia's Covid data")
+    st.write("The data for this app was acquired from [The COVID Tracking Project](https://covidtracking.com/), where every day volunteers are ensuring that the public has the best available Covid data.")
+
+    if st.checkbox("Show Data"):
+        st.dataframe(covid_data)
+        st.write("Dataset contains %s columns and %s rows" % (len(covid_data.columns),len(covid_data)))
+
+    # Positive increase
     pos_inc_fig = go.Figure()
     pos_inc_fig.update_layout(covid_template)
     pos_inc_fig.update_layout(
         #template=covid_template,
         title="Daily increase of positive cases",
         xaxis_title='Date',
-        yaxis_title='Positive Increase',
+        yaxis_title='Daily Positive Increase',
         legend_title="Legend",
         xaxis=slider_template
         )
-    pos_inc_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positiveIncrease'],name='positiveIncrease',text=covid_data['positiveIncrease'],textposition='inside',))
+    pos_inc_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positiveIncrease'],name='Positive Increase',text=covid_data['positiveIncrease'],textposition='inside',))
     
     moving_avg_7 = covid_data['positiveIncrease'].rolling(7).mean()
     pos_inc_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
-
     pos_inc_fig.update_yaxes(automargin=True)
     
     st.plotly_chart(pos_inc_fig, use_container_width=False)
 
 
-    #TODO Selectbox Positive Cumulative
     if(st.checkbox("Total Positive")):
+        # Total Positive
         pos_cumu_fig = go.Figure()
         pos_cumu_fig.update_layout(covid_template)
         pos_cumu_fig.update_layout(
@@ -124,7 +125,7 @@ if(choose_worktype == 'Key metrics'):
             legend_title="Legend",
             xaxis=slider_template
             )
-        pos_cumu_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positive'],name='positive',text=covid_data['positive'],textposition='inside',))
+        pos_cumu_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positive'],name='Positive',text=covid_data['positive'],textposition='inside',))
     
         moving_avg_7 = covid_data['positive'].rolling(7).mean()
         pos_cumu_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
@@ -133,11 +134,10 @@ if(choose_worktype == 'Key metrics'):
     
         st.plotly_chart(pos_cumu_fig, use_container_width=False)
 
-    #TODO Death Increase
+    # Death Increase
     dth_inc_fig = go.Figure()
     dth_inc_fig.update_layout(covid_template)
     dth_inc_fig.update_layout(
-        #template=covid_template,
         title="Daily Increase of Deaths",
         xaxis_title='Date',
         yaxis_title='Death Increase',
@@ -152,19 +152,18 @@ if(choose_worktype == 'Key metrics'):
     dth_inc_fig.update_yaxes(automargin=True)
     
     st.plotly_chart(dth_inc_fig, use_container_width=False)
-    #TODO Selectbox Death Cumulative
+    # Selectbox Death Cumulative
     if(st.checkbox("Death Cumulative")):
         dth_cumu_fig = go.Figure()
         dth_cumu_fig.update_layout(covid_template)
         dth_cumu_fig.update_layout(
-            #template=covid_template,
             title="Total Deaths",
             xaxis_title='Date',
             yaxis_title='Deaths Cumulative',
             legend_title="Legend",
             xaxis=slider_template
             )
-        dth_cumu_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['death'],name='death',text=covid_data['death'],textposition='inside',))
+        dth_cumu_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['death'],name='Death Count',text=covid_data['death'],textposition='inside',))
     
         moving_avg_7 = covid_data['death'].rolling(7).mean()
         dth_cumu_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
@@ -173,11 +172,10 @@ if(choose_worktype == 'Key metrics'):
     
         st.plotly_chart(dth_cumu_fig, use_container_width=False)
 
-    #TODO Hospitalized Increase
+    # Hospitalized Increase
     hosp_inc_fig = go.Figure()
     hosp_inc_fig.update_layout(covid_template)
     hosp_inc_fig.update_layout(
-        #template=covid_template,
         title="Daily Increase of Hospitalizations",
         xaxis_title='Date',
         yaxis_title='Hospitalization Increase',
@@ -193,18 +191,17 @@ if(choose_worktype == 'Key metrics'):
     
     st.plotly_chart(hosp_inc_fig, use_container_width=False)
 
-    #TODO Hospitalized Currently
+    # Hospitalized Currently
     hosp_cur_fig = go.Figure()
     hosp_cur_fig.update_layout(covid_template)
     hosp_cur_fig.update_layout(
-        #template=covid_template,
         title="Currently Hospitalized",
         xaxis_title='Date',
         yaxis_title='Hospitalized',
         legend_title="Legend",
         xaxis=slider_template
         )
-    hosp_cur_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['hospitalizedCurrently'],name='Death Increase',text=covid_data['hospitalizedCurrently'],textposition='inside',))
+    hosp_cur_fig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['hospitalizedCurrently'],name='Hosptialized Currently',text=covid_data['hospitalizedCurrently'],textposition='inside',))
     
     moving_avg_7 = covid_data['hospitalizedCurrently'].rolling(7).mean()
     hosp_cur_fig.add_trace(go.Scatter(x=covid_data['date'],y=moving_avg_7, name="7-Day average"))
@@ -213,12 +210,11 @@ if(choose_worktype == 'Key metrics'):
     
     st.plotly_chart(hosp_cur_fig, use_container_width=False)
 
-    #TODO Selectbox Hospitalized Cumulative 
+    # Selectbox Hospitalized Cumulative 
     if(st.checkbox("Hospitalized Cumulative")):
         hosp_cumu_fig = go.Figure()
         hosp_cumu_fig.update_layout(covid_template)
         hosp_cumu_fig.update_layout(
-            #template=covid_template,
             title="Total Hospitalizations",
             xaxis_title='Date',
             yaxis_title='Hospitalized Cumulative',
@@ -233,181 +229,7 @@ if(choose_worktype == 'Key metrics'):
         hosp_cumu_fig.update_yaxes(automargin=True)
     
         st.plotly_chart(hosp_cumu_fig, use_container_width=False)
-        
 
-
-
-
-
-
-
-
-
-
-
-if(choose_worktype == 'Play'):
-    choose_x = st.sidebar.selectbox("Choose the x-data to plot",list(covid_data.columns))
-    choose_y= st.sidebar.selectbox("Choose the y-data to plot",list(covid_data.columns))#['New_Deaths','New_Hospitalized',])
-
-    #st.subheader('Georgia Covid data from March 3rd to Present')
-    #st.write(covid_data)
-
-    st.header('Data Experimentation')
-
-    #fig = px.bar(covid_data, x=choose_x,y=choose_y,text=choose_y,)
-
-    #st.plotly_chart(fig, use_container_width=True)
-    #lineFigure = st.checkbox("Line Figure")
-    if st.checkbox("Line Figure"):
-        lineFig = px.line(covid_data, x=choose_x, y=choose_y, height=450,width=600)
-        lineFig.update_layout(font=dict(size=15))
-        st.plotly_chart(lineFig, use_container_width=True)
-    moving_avg_7 = covid_data[choose_y].rolling(7).mean()
-    if st.checkbox("Scatter Figure"):
-        scatterFig = go.Figure()
-        scatterFig.add_trace(go.Scatter(x=covid_data[choose_x],y=covid_data[choose_y], name=choose_y ,mode="lines+markers+text"))
-
-        scatterFig.update_layout(
-        
-        title="Scatter plot of %s by %s" % (choose_y,choose_x),
-        xaxis_title=choose_x,
-        yaxis_title=choose_y,
-        legend_title="Legend",
-        font=dict(
-            #family="Courier New, monospace",
-            size=15,
-            color="RebeccaPurple"
-        ),
-        height=500,
-        width=900
-        )
-        scatterFig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                    
-                    dict(count=7,
-                        label="Weekly",
-                        step="day",
-                        stepmode="todate"),
-                    dict(count=14,
-                        label="Biweekly",
-                        step="day",
-                        stepmode="backward"),
-                    dict(count=1,
-                        label="Monthly",
-                        step="month",
-                        stepmode="backward"),
-                    dict(count=1,
-                        label="YTD",
-                        step="year",
-                        stepmode="todate"),
-                    dict(step="all")
-                ])
-            ),
-            rangeslider=dict(
-                visible=True
-            ),
-            type="date"
-        )
-        )
-
-        #scatterFig.update_layout(
-        #autosize=False, 
-        #height=450,
-        #width=600)
-
-        #moving_avg_7 = covid_data[choose_y].rolling(7).mean()
-        scatterFig.add_trace(go.Scatter(x=covid_data[choose_x],y=moving_avg_7, name="7-Day moving average"))
-        scatterFig.update_yaxes(automargin=True)
-        st.plotly_chart(scatterFig, use_container_width=False)
-    if st.checkbox("Bar Figure"):
-        barFig = go.Figure()
-        barFig.add_trace(go.Bar(x=covid_data[choose_x],y=covid_data[choose_y],name=choose_y,text=covid_data[choose_y],textposition='inside',))
-
-        barFig.add_trace(go.Scatter(x=covid_data[choose_x],y=moving_avg_7, name="7-Day moving average"))
-        barFig.update_layout(
-        title="Bar plot of %s by %s" % (choose_y,choose_x),
-        xaxis_title=choose_x,
-        yaxis_title=choose_y,
-        legend_title="Legend",
-        font=dict(
-            #family="Courier New, monospace",
-            size=15,
-            color="RebeccaPurple"
-        ),
-        height=700,
-        width=1000,
-        xaxis=slider_template
-        )
-        barFig.update_yaxes(automargin=True)
-        st.plotly_chart(barFig, use_container_width=False)
-
-    if st.checkbox("Subplots"):
-
-        subFig = make_subplots(rows=1,cols=2)
-        #moving_avg_7_ = covid_data[choose_y].rolling(7).mean()
-        #subFig.add_trace(go.Scatter(x=covid_data['positive'],y=moving_avg_7_, name="7-Day moving average"))
-        
-
-        subFig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positive'],name='Positive cases',text=covid_data['positive'],textposition='inside',),row=1, col=1)
-
-        subFig.add_trace(go.Bar(x=covid_data['date'],y=covid_data['positiveIncrease'],name='Positive increase',text=covid_data['positiveIncrease'],textposition='inside',),row=1, col=2)
-
-        subFig.update_layout(
-        title="Bar plot of %s by %s" % (choose_y,choose_x),
-        xaxis_title=choose_x,
-        yaxis_title=choose_y,
-        legend_title="Legend",
-        font=dict(
-            #family="Courier New, monospace",
-            size=15,
-            color="RebeccaPurple"
-        ),
-        height=700,
-        width=1000,
-        )
-        subFig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                    
-                    dict(count=7,
-                        label="Weekly",
-                        step="day",
-                        stepmode="todate"),
-                    dict(count=14,
-                        label="Biweekly",
-                        step="day",
-                        stepmode="backward"),
-                    dict(count=1,
-                        label="Monthly",
-                        step="month",
-                        stepmode="backward"),
-                    dict(count=1,
-                        label="YTD",
-                        step="year",
-                        stepmode="todate"),
-                    dict(step="all")
-                ])
-            ),
-            rangeslider=dict(
-                visible=True
-            ),
-            type="date"
-        )
-        )
-        subFig.update_yaxes(automargin=True)
-
-        st.plotly_chart(subFig, use_container_width=False)
-
-
-
-
-
-    #scatterFig.add_trace(go.Scatter(x=covid_data['date'],y=covid_data['positive'], mode="lines+markers"))
-
-    #st.plotly_chart(lineFig, use_container_width=True)
-    #st.plotly_chart(scatterFig, use_container_width=True)
-    #st.plotly_chart(barFig, use_container_width=True)
-
+if(sections == 'Future Goals'):
+    st.title("Future Goals")
+    st.markdown("<ul><b><li>Add state selection <li>Include national average comparisons within graphs <ul> ",unsafe_allow_html=True)
